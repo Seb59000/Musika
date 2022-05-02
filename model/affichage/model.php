732501f85039338b314f1364078c7fffad093f4f
Seb59000 <36163352+Seb59000@users.noreply.github.com>
2022-05-02 12:06:55 +0200

    Update model.php

--- model/affichage/model.php
+++ model/affichage/model.php
@@ -1,15 +1,13 @@
 <?php
-/**
- * function execute($requeteSQL) {
- */
-    try
-    {
+
+function execute($requeteSQL)
+{
+
+    try {
         $connexionBDD = new PDO('mysql:host=localhost;dbname=musikalassault;charset=utf8', 'root', '');
-    }
-    catch (Exception $e)
-    {
+    } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
-    }       
+    }
     $resultat = $connexionBDD->query($requeteSQL);
     return $resultat;
 }
@@ -17,12 +15,12 @@
  * [afficherNbPhotos affiche le Nb de Photos]
  * @return [int] [count]
  */
-function afficherNbPhotos() {
+function afficherNbPhotos()
+{
     $resultat = execute("CALL `afficherNbPhotos`()");  // from accueil
     $count = 2;
-    while ($donnees = $resultat->fetch())
-    {            
-        $count = $donnees['nb'];     
+    while ($donnees = $resultat->fetch()) {
+        $count = $donnees['nb'];
     }
     return $count;
 }
@@ -30,21 +28,21 @@ function afficherNbPhotos() {
  * [afficherPhotosAccueil affiche les Photos de la page d'Accueil]
  * @return [Array] [arrayPhotos]
  */
-function afficherPhotosAccueil() { 
-    $resultat = execute("CALL `afficherPhotosAccueil`()"); 
+function afficherPhotosAccueil()
+{
+    $resultat = execute("CALL `afficherPhotosAccueil`()");
 
     $arrayPhotos = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idPhoto = $donnees['idphoto']; 
-        $pseudoExpediteur = $donnees['pseudo']; 
-        $type = $donnees['type']; 
-        $date = $donnees['date']; 
-        $commentaire = $donnees['commentaire']; 
+    while ($donnees = $resultat->fetch()) {
+        $idPhoto = $donnees['idphoto'];
+        $pseudoExpediteur = $donnees['pseudo'];
+        $type = $donnees['type'];
+        $date = $donnees['date'];
+        $commentaire = $donnees['commentaire'];
 
-        $photo = new Photo($idPhoto, $pseudoExpediteur, $type, $date, $commentaire); 
+        $photo = new Photo($idPhoto, $pseudoExpediteur, $type, $date, $commentaire);
 
         $arrayPhotos[$index] = $photo;
         $index++;
@@ -55,26 +53,26 @@ function afficherPhotosAccueil() {
  * [afficherUtilisateursBureau affiche les UtilisateursBureau]
  * @return [Array] [arrayUtilisateur]
  */
-function afficherUtilisateursBureau() {
+function afficherUtilisateursBureau()
+{
     $resultat = execute("CALL `afficherUtilisateursBureau`()");
     $arrayUtilisateur = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idUtilisateur = $donnees['idUtilisateur']; 
-        $idPhoto = $donnees['idPhoto']; 
-        $typePhoto = $donnees['typePhoto']; 
-        $pseudo = $donnees['pseudo']; 
-        $idBureau = $donnees['idBureau']; 
+    while ($donnees = $resultat->fetch()) {
+        $idUtilisateur = $donnees['idUtilisateur'];
+        $idPhoto = $donnees['idPhoto'];
+        $typePhoto = $donnees['typePhoto'];
+        $pseudo = $donnees['pseudo'];
+        $idBureau = $donnees['idBureau'];
         $statut = $donnees['statut'];
         $fonction = $donnees['fonction'];
-        $adresseTwitter = $donnees['adresseTwitter']; 
-        $adresseFacebook = $donnees['adresseFacebook']; 
-        $adresseGooglePlus = $donnees['adresseGooglePlus']; 
-        $adresseLinkedin = $donnees['adresseLinkedin']; 
-        
-        $utilisateur = new UtilisateurBureau($idUtilisateur, $idPhoto, $typePhoto, $pseudo, $statut, $idBureau, $fonction, $adresseTwitter, $adresseFacebook, $adresseGooglePlus, $adresseLinkedin); 
+        $adresseTwitter = $donnees['adresseTwitter'];
+        $adresseFacebook = $donnees['adresseFacebook'];
+        $adresseGooglePlus = $donnees['adresseGooglePlus'];
+        $adresseLinkedin = $donnees['adresseLinkedin'];
+
+        $utilisateur = new UtilisateurBureau($idUtilisateur, $idPhoto, $typePhoto, $pseudo, $statut, $idBureau, $fonction, $adresseTwitter, $adresseFacebook, $adresseGooglePlus, $adresseLinkedin);
 
         $arrayUtilisateur[$index] = $utilisateur;
         $index++;
@@ -87,13 +85,13 @@ function afficherUtilisateursBureau() {
  * @param  [type] $categorie [description]
  * @return [type]            [description]
  */
-function afficherNbAnnonces($categorie) {
-    $sql = "SET @p0='".$categorie."'; CALL `afficherNbAnnonces`(@p0);";
+function afficherNbAnnonces($categorie)
+{
+    $sql = "SET @p0='" . $categorie . "'; CALL `afficherNbAnnonces`(@p0);";
     $resultat = execute($sql);  // from tableauAnnoncesAutres
     $count = 2;
-    while ($donnees = $resultat->fetch())
-    {            
-        $count = $donnees['nb'];     
+    while ($donnees = $resultat->fetch()) {
+        $count = $donnees['nb'];
     }
     return $count;
 }
@@ -102,23 +100,23 @@ function afficherNbAnnonces($categorie) {
  * @param  [type] $indexPremierAnnonceAutres [description]
  * @return [type]                            [description]
  */
-function afficherAnnoncesAutres($indexPremierAnnonceAutres) {
-    $sql = "SET @p0='".$indexPremierAnnonceAutres."'; CALL `afficherAnnoncesAutres`(@p0);";
+function afficherAnnoncesAutres($indexPremierAnnonceAutres)
+{
+    $sql = "SET @p0='" . $indexPremierAnnonceAutres . "'; CALL `afficherAnnoncesAutres`(@p0);";
     $resultat = execute($sql);  // from utilisateur/tableauAnnoncesAutres.php
     $arrayAnnoncesAutres = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idAnnonce = $donnees['idAnnonce']; 
-        $categorie = $donnees['categorie']; 
-        $idExpediteur = $donnees['idExpediteur']; 
-        $titre = $donnees['titre']; 
-        $contenu = $donnees['contenu']; 
-        $date = $donnees['date']; 
-        $type_photo = $donnees['type_photo']; 
+    while ($donnees = $resultat->fetch()) {
+        $idAnnonce = $donnees['idAnnonce'];
+        $categorie = $donnees['categorie'];
+        $idExpediteur = $donnees['idExpediteur'];
+        $titre = $donnees['titre'];
+        $contenu = $donnees['contenu'];
+        $date = $donnees['date'];
+        $type_photo = $donnees['type_photo'];
 
-        $annonceAutres = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo); 
+        $annonceAutres = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo);
 
         $arrayAnnoncesAutres[$index] = $annonceAutres;
         $index++;
@@ -131,23 +129,23 @@ function afficherAnnoncesAutres($indexPremierAnnonceAutres) {
  * @param  [type] $indexPremierAnnonceConcert [description]
  * @return [type]                             [description]
  */
-function afficherAnnoncesConcerts($indexPremierAnnonceConcert) { 
-    $sql = "SET @p0='".$indexPremierAnnonceConcert."'; CALL `afficherAnnoncesConcerts`(@p0);";
+function afficherAnnoncesConcerts($indexPremierAnnonceConcert)
+{
+    $sql = "SET @p0='" . $indexPremierAnnonceConcert . "'; CALL `afficherAnnoncesConcerts`(@p0);";
     $resultat = execute($sql);  // from utilisateur/tableauAnnoncesAutres.php
     $arrayAnnoncesConcerts = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idAnnonce = $donnees['idAnnonce']; 
-        $categorie = $donnees['categorie']; 
-        $idExpediteur = $donnees['idExpediteur']; 
-        $titre = $donnees['titre']; 
-        $contenu = $donnees['contenu']; 
-        $date = $donnees['date']; 
-        $type_photo = $donnees['type_photo']; 
+    while ($donnees = $resultat->fetch()) {
+        $idAnnonce = $donnees['idAnnonce'];
+        $categorie = $donnees['categorie'];
+        $idExpediteur = $donnees['idExpediteur'];
+        $titre = $donnees['titre'];
+        $contenu = $donnees['contenu'];
+        $date = $donnees['date'];
+        $type_photo = $donnees['type_photo'];
 
-        $annonceConcerts = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo); 
+        $annonceConcerts = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo);
 
         $arrayAnnoncesConcerts[$index] = $annonceConcerts;
         $index++;
@@ -159,23 +157,23 @@ function afficherAnnoncesConcerts($indexPremierAnnonceConcert) {
  * @param  [type] $indexPremiereAnnonce [description]
  * @return [type]                       [description]
  */
-function afficherAnnoncesMatos($indexPremiereAnnonce) {
-    $sql = "SET @p0='".$indexPremiereAnnonce."'; CALL `afficherAnnoncesMatos`(@p0);";
+function afficherAnnoncesMatos($indexPremiereAnnonce)
+{
+    $sql = "SET @p0='" . $indexPremiereAnnonce . "'; CALL `afficherAnnoncesMatos`(@p0);";
     $resultat = execute($sql);  // from utilisateur/tableauAnnoncesAutres.php
     $arrayAnnoncesMatos = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idAnnonce = $donnees['idAnnonce']; 
-        $categorie = $donnees['categorie']; 
-        $idExpediteur = $donnees['idExpediteur']; 
-        $titre = $donnees['titre']; 
-        $contenu = $donnees['contenu']; 
-        $date = $donnees['date']; 
-        $type_photo = $donnees['type_photo']; 
+    while ($donnees = $resultat->fetch()) {
+        $idAnnonce = $donnees['idAnnonce'];
+        $categorie = $donnees['categorie'];
+        $idExpediteur = $donnees['idExpediteur'];
+        $titre = $donnees['titre'];
+        $contenu = $donnees['contenu'];
+        $date = $donnees['date'];
+        $type_photo = $donnees['type_photo'];
 
-        $annonceMatos = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo); 
+        $annonceMatos = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo);
 
         $arrayAnnoncesMatos[$index] = $annonceMatos;
         $index++;
@@ -188,23 +186,23 @@ function afficherAnnoncesMatos($indexPremiereAnnonce) {
  * @param  [type] $indexPremiereAnnonce [description]
  * @return [type]                       [description]
  */
-function afficherAnnoncesMusiciens($indexPremiereAnnonce) {
-    $sql = "SET @p0='".$indexPremiereAnnonce."'; CALL `afficherAnnoncesMusiciens`(@p0);";
+function afficherAnnoncesMusiciens($indexPremiereAnnonce)
+{
+    $sql = "SET @p0='" . $indexPremiereAnnonce . "'; CALL `afficherAnnoncesMusiciens`(@p0);";
     $resultat = execute($sql);  // from utilisateur/tableauAnnoncesAutres.php
     $arrayAnnoncesMusiciens = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idAnnonce = $donnees['idAnnonce']; 
-        $categorie = $donnees['categorie']; 
-        $idExpediteur = $donnees['idExpediteur']; 
-        $titre = $donnees['titre']; 
-        $contenu = $donnees['contenu']; 
-        $date = $donnees['date']; 
-        $type_photo = $donnees['type_photo']; 
+    while ($donnees = $resultat->fetch()) {
+        $idAnnonce = $donnees['idAnnonce'];
+        $categorie = $donnees['categorie'];
+        $idExpediteur = $donnees['idExpediteur'];
+        $titre = $donnees['titre'];
+        $contenu = $donnees['contenu'];
+        $date = $donnees['date'];
+        $type_photo = $donnees['type_photo'];
 
-        $annonceMusiciens = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo); 
+        $annonceMusiciens = new Annonce($idAnnonce, $categorie, $idExpediteur, $titre, $contenu, $date, $type_photo);
 
         $arrayAnnoncesMusiciens[$index] = $annonceMusiciens;
         $index++;
@@ -216,12 +214,12 @@ function afficherAnnoncesMusiciens($indexPremiereAnnonce) {
  * [afficherNbBonsPlans description]
  * @return [type] [description]
  */
-function afficherNbBonsPlans() {
+function afficherNbBonsPlans()
+{
     $resultat = execute("CALL `afficherNbBonsPlans`()");  // from tableauAnnoncesAutres
     $count = 2;
-    while ($donnees = $resultat->fetch())
-    {            
-        $count = $donnees['nb'];     
+    while ($donnees = $resultat->fetch()) {
+        $count = $donnees['nb'];
     }
     return $count;
 }
@@ -230,22 +228,22 @@ function afficherNbBonsPlans() {
  * @param  [type] $indexPremierBonPlan [description]
  * @return [type]                      [description]
  */
-function afficherBonPlan($indexPremierBonPlan) {
-    $sql = "SET @p0='".$indexPremierBonPlan."'; CALL `afficherBonPlan`(@p0);";
+function afficherBonPlan($indexPremierBonPlan)
+{
+    $sql = "SET @p0='" . $indexPremierBonPlan . "'; CALL `afficherBonPlan`(@p0);";
     $resultat = execute($sql);
     $arrayBonPlan = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idBonPlan = $donnees['idBonPlan']; 
-        $titre = $donnees['titre']; 
-        $description = $donnees['description']; 
-        $adresse = $donnees['adresse']; 
-        $lienWeb = $donnees['lienWeb']; 
-        $numTel = $donnees['numTel']; 
+    while ($donnees = $resultat->fetch()) {
+        $idBonPlan = $donnees['idBonPlan'];
+        $titre = $donnees['titre'];
+        $description = $donnees['description'];
+        $adresse = $donnees['adresse'];
+        $lienWeb = $donnees['lienWeb'];
+        $numTel = $donnees['numTel'];
 
-        $bonPlan = new BonPlan($idBonPlan, $titre, $description, $adresse, $lienWeb, $numTel); 
+        $bonPlan = new BonPlan($idBonPlan, $titre, $description, $adresse, $lienWeb, $numTel);
 
         $arrayBonPlan[$index] = $bonPlan;
         $index++;
@@ -258,22 +256,22 @@ function afficherBonPlan($indexPremierBonPlan) {
  * @param  [type] $indexPremierePhoto [description]
  * @return [type]                     [description]
  */
-function afficherPhotos($indexPremierePhoto) { 
-    $sql = "SET @p0='".$indexPremierePhoto."'; CALL `afficherPhotos`(@p0);";
+function afficherPhotos($indexPremierePhoto)
+{
+    $sql = "SET @p0='" . $indexPremierePhoto . "'; CALL `afficherPhotos`(@p0);";
     $resultat = execute($sql); //from accueil
 
     $arrayPhotos = null;
-    $index=0;
+    $index = 0;
 
-    while ($donnees = $resultat->fetch())
-    {    
-        $idPhoto = $donnees['idphoto']; 
-        $pseudoExpediteur = $donnees['pseudo']; 
-        $type = $donnees['type']; 
-        $date = $donnees['date']; 
-        $commentaire = $donnees['commentaire']; 
+    while ($donnees = $resultat->fetch()) {
+        $idPhoto = $donnees['idphoto'];
+        $pseudoExpediteur = $donnees['pseudo'];
+        $type = $donnees['type'];
+        $date = $donnees['date'];
+        $commentaire = $donnees['commentaire'];
 
-        $photo = new Photo($idPhoto, $pseudoExpediteur, $type, $date, $commentaire); 
+        $photo = new Photo($idPhoto, $pseudoExpediteur, $type, $date, $commentaire);
 
         $arrayPhotos[$index] = $photo;
         $index++;
